8f19ed5fb7ca98aa540207cc6da61349a984b1ba
Eirado <GABEIRADO2@HOTMAIL.COm>
2024-03-13 09:25:34 -0300

    .

---
+++ exemplo/mathOperations.js
@@ -0,0 +1,27 @@
+const add = (a, b) => {
+	return a + b
+}
+
+const subtration = (a, b) => {
+	return a - b
+}
+
+const division  = (a,b) => {
+	return a / b
+}
+
+const multi = (a,b) =>{
+	return a * b
+}
+
+console.log(add(4, 6))
+
+var num1 = 10
+var num2 = 20
+
+console.log(`resultado da + entre ${num1} e ${num2} e ${num1 + num2}` )
+
+console.log(add(num1, num2))
+console.log(subtration(num1, num2))
+console.log(division(num1, num2))
+console.log(multi(num1, num2))
