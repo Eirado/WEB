1581494a85f0c84d7352a0fabb85cc46c9464355
Eirado <GABEIRADO2@HOTMAIL.COm>
2024-03-13 11:52:34 -0300

    .

--- Atividade2/verificarNumeroPrimo.js
+++ Atividade2/verificarNumeroPrimo.js
@@ -1,12 +1,23 @@
-function verificarNumeroPrimo(num1){
-    if (num1 <= 1){
-        return
+function verificarNumeroPrimo(n){
+    if (n <= 1) {
+        return false;
     }
-
-    if (num1 % 2 == 0) {
-        return console.log("True")
-    } else
-    return console.log("False")
+    for (i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+    
 }
+console.log(verificarNumeroPrimo(0));        
+console.log(verificarNumeroPrimo(1));        
+console.log(verificarNumeroPrimo(2));        
+console.log(verificarNumeroPrimo(3));        
+console.log(verificarNumeroPrimo(7));        
+console.log(verificarNumeroPrimo(83));      
+console.log(verificarNumeroPrimo(100));    
+console.log(verificarNumeroPrimo(991));    
+console.log(verificarNumeroPrimo(104729));  
+console.log(verificarNumeroPrimo(14348907));
 
-verificarNumeroPrimo(2)
