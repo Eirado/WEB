7e8ea05c1a2cce42d55ed58eb4074cf51aeefb4b
Eirado <GABEIRADO2@HOTMAIL.COm>
2024-03-13 09:30:45 -0300

    .

--- exemplo/mathOperations.js
+++ exemplo/mathOperations.js
@@ -18,8 +18,9 @@ console.log(add(4, 6))
 
 var num1 = 10
 var num2 = 20
+var soma = add(num1, num2)
 
-console.log(`resultado da + entre ${num1} e ${num2} e ${num1 + num2}` )
+console.log(`resultado da soma entre ${num1} e ${num2} e ${soma}`)
 
 console.log(add(num1, num2))
 console.log(subtration(num1, num2))
